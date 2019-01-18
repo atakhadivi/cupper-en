c394cad15f2a3e08fb04fe8adf93f9b6a832003e
atakhadivi <atakhadivi@gmail.com>
2019-01-18 11:04:46 +0330

    Add files via upload

---
+++ logo/logo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+def logo():
+    print "\33[31m-------------------------------------------------------------------"
+    print "\r\n"
+    print "        \33[31mcccccccc                "
+    print "       \33[33mu        \33[33mu               \33[37m#name:Cupper"
+    print "      \33[33mu          \33[33mu              \33[37m#Author:Ata Khadivi,Saferman"
+    print "     \33[33mu   \33[7m):S:(\33[0m    \33[33mu             \33[37m#Introduction:           "
+    print "      \33[32mp          \33[32mp                   \33[37m......have nothing to say......"
+    print "         \33[32mp    \33[32mp                 \33[37m#Contact information:*******:)"
+    print "           \33[34mer"
+    print "\r\n"
+    print "-------------------------------------------------------------------"
+    print "\r\n"
